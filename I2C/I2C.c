586100c9cabef202429e33793b9620f2cdb385ff
ammar shahin <ammar0ammar612@gmail.com>
2019-07-08 13:07:03 +0200

    updating the I2C Driver

--- I2C/I2C.c
+++ I2C/I2C.c
@@ -1,17 +1,34 @@
+/*
+ * I2C.h
+ *
+ * Created: 4/24/2019 6:45:32 PM
+ *  Author: ammar shahin
+ */
+ 
 #include "I2C.h"
 
 
 void I2C_master_init(void)
 {
-	TWBR = (uint8_t)TWBR_val;
+	TWBR = (TU08)TWBR_val; // Set the bit rate
+	
+	/* enable the TWI ack, Interrupt enable, and TWI enable */
+	TWCR |= (1<<TWEA) | (1<<TWEN);
 }
 
-void I2C_start(uint8_t address)
+
+void I2C_Slave_Init(TU08 My_Add){
+	TWAR = My_Add<<1;
+	TWCR = (1<<TWEA) | (1<<TWEN);
+}
+
+
+void I2C_start(TU08 address)
 {
 	// reset TWI control register
 	TWCR = 0;
 	// transmit START condition
-	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
+	TWCR |=  (1<<TWSTA) | (1<<TWEN);
 	// wait for end of transmission
 	while( !(TWCR & (1<<TWINT)) );
 	
@@ -23,7 +40,7 @@ void I2C_start(uint8_t address)
 	while( !(TWCR & (1<<TWINT)) );
 }
 
-void I2C_write(uint8_t data)
+void I2C_write(TU08 data)
 {
 	// load data into data register
 	TWDR = data;
@@ -33,7 +50,7 @@ void I2C_write(uint8_t data)
 	while( !(TWCR & (1<<TWINT)) );
 }
 
-uint8_t I2C_read(void)
+TU08 I2C_read(void)
 {
 
 	// start TWI module and acknowledge data after reception
@@ -49,12 +66,8 @@ void I2C_stop(void)
 	// transmit STOP condition
 	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
 }
-void I2C_Slave_Init(uint8_t My_Add){
-	TWAR = My_Add<<1;
-	TWCR = (1<<TWEA) | (1<<TWINT) | (1<<TWEN);
-}
 
-uint8_t I2C_Slave_avialable(){	
+TU08 I2C_Slave_avialable(){	
 	TWCR |= (1<<TWINT) | (1<<TWEN);
 	while (!(TWCR & (1<<TWINT)));
 	return ((TWSR & 0xF8) == TW_SR_SLA_ACK); 
--- I2C/I2C.h
+++ I2C/I2C.h
@@ -4,22 +4,39 @@
  * Created: 4/24/2019 6:45:32 PM
  *  Author: ammar shahin
  */
+ 
 #ifndef I2C_MASTER_H
 #define I2C_MASTER_H
 
-#include "std_macros.h"
-#include <util/twi.h>
+#include <util/twi.h> 
+#include "BasicTypes.h"
+#include "BitMasking_Macros.h"
 
 #define F_SCL 100000UL // SCL frequency
+#define F_CPU 1000000UL
 
 #define Prescaler 1
 #define TWBR_val ((((F_CPU / F_SCL) / Prescaler) - 16 ) / 2)
 
+
+#define TW_MR_ARB_LOST   0x38
+#define TW_START         0x08  // start bit has been transmitted
+#define TW_REP_START     0x10
+#define TW_MT_SLA_ACK    0x18  // +ve ACK for address transmitted
+#define TW_MT_DATA_ACK   0x28
+#define TW_MR_DATA_NACK  0x58
+#define TW_MR_SLA_NACK   0x48
+#define TW_MT_SLA_NACK   0x20
+#define TW_MT_DATA_NACK  0x30
+#define TW_MR_DATA_ACK   0x50
+#define TW_MR_SLA_ACK    0x40
+
+
 void I2C_master_init(void);
-void I2C_start(uint8_t address);
-void I2C_write(uint8_t data);
-uint8_t I2C_read(void);
+void I2C_start(TU08 address);
+void I2C_write(TU08 data);
+TU08 I2C_read(void);
 void I2C_stop(void);
-uint8_t I2C_Slave_avialable();
-void I2C_Slave_Init(uint8_t My_Add);
+TU08 I2C_Slave_avialable();
+void I2C_Slave_Init(TU08 My_Add);
 #endif // I2C_MASTER_H
